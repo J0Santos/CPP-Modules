9b0b6b3353b430a671d59dd9f4b008f1339f3ed1
Jose pedro Santos <zepedroabsantos@gmail.com>
2022-06-08 18:03:29 +0100

    started ex04

---
+++ CPP01/ex04/incs/Replace.hpp
@@ -0,0 +1,10 @@
+#ifndef REPLACE_HPP
+# define REPLACE_HPP
+
+#include <iostream>
+
+# ifndef LOG
+#  define LOG(x) std::cout << x << std::endl
+# endif
+
+#endif
---
+++ CPP01/ex04/srcs/main.cpp
@@ -0,0 +1,11 @@
+#include "Replace.hpp"
+
+int	main (int argc, char **argv)
+{
+
+	(void)argv;
+	if (argc != 4)
+		LOG("Arguments can only be:\n\t-> <Filename>\n\t-> <s1> (to be replaced)\n\t-> <s2> (to replace with)");
+	
+		
+}
