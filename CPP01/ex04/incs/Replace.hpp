142bca67646f56313990f809e08aa073f294c744
Jose pedro Santos <zepedroabsantos@gmail.com>
2022-06-09 18:53:23 +0100

    need to replace << with get() so iostream finds white-space chars

--- CPP01/ex04/incs/Replace.hpp
+++ CPP01/ex04/incs/Replace.hpp
@@ -2,9 +2,13 @@
 # define REPLACE_HPP
 
 #include <iostream>
+#include <string>
+#include <fstream>
 
 # ifndef LOG
 #  define LOG(x) std::cout << x << std::endl
 # endif
+
+std::string	do_replace(std::string str, std::string before, std::string after);
 
 #endif
--- CPP01/ex04/srcs/main.cpp
+++ CPP01/ex04/srcs/main.cpp
@@ -1,11 +1,48 @@
 #include "Replace.hpp"
 
+std::string	do_replace(std::string str, std::string before, std::string after)
+{
+	size_t	find = -1;
+	size_t	pos = 0;
+
+	LOG(str);
+	while ((find = str.find(before, pos)) != std::string::npos)
+	{
+			pos += find;
+			str.erase(find, before.length());
+			str.insert(find, after);
+	}
+	return (str);
+}
+
 int	main (int argc, char **argv)
 {
+	std::string	content;
+	std::string	newContent;
+	std::string	fileType = ".replace";
+	std::string	newFileName;
 
-	(void)argv;
 	if (argc != 4)
+	{
 		LOG("Arguments can only be:\n\t-> <Filename>\n\t-> <s1> (to be replaced)\n\t-> <s2> (to replace with)");
-	
-		
+		exit(EXIT_FAILURE);
+	}
+	std::ifstream file;
+	file.open(argv[1], std::ios::in);
+	if (!file)
+	{
+		std::cerr << "Error - file could not be opened" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	newFileName = argv[1] + fileType;
+	std::fstream newFile;
+	newFile.open(newFileName, std::ios::app);
+	while (file >> content)
+	{
+		newContent = do_replace(content, argv[2], argv[3]);
+		newContent.insert(newContent.length(), "\n");
+		newFile << newContent;
+	}
+	newFile.close();
+	file.close();
 }
