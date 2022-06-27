5f7ae1128879ea599e5cd82d294cd946d952d0a0
J0Santos <zepedroabsantos@gmail.com>
2022-06-27 13:22:47 +0100

    need to handle 0 division error

--- CPP02/ex03/srcs/bsp.cpp
+++ CPP02/ex03/srcs/bsp.cpp
@@ -2,8 +2,24 @@
 
 bool    bsp(Point const a, Point const b, Point const c, Point const point)
 {
-    
+	Fixed	alpha;
+	Fixed	alphaD;
+	Fixed	beta;
+	Fixed	gamma;
+
+    std::cout << (b.getY() - c.getY()) * (point.getX() - c.getX()) << std::endl;
+    std::cout << (c.getX() - b.getX()) * (point.getY() - c.getY()) << std::endl;
 
 
-    return 0;
+
+	alpha = ((b.getY() - c.getY()) * (point.getX() - c.getX()) + (c.getX() - b.getX()) * (point.getY() - c.getY())) ;
+	alphaD = ((b.getY() - c.getY()) * (a.getX() - c.getX()) + (c.getX() - b.getX()) * (a.getY() - c.getY()));
+
+	std::cout << alpha << std::endl;
+	std::cout << alphaD << std::endl;
+	// beta = ((c.getY() - a.getY()) * (point.getX() - c.getX()) + (a.getX() - c.getX()) * (point.getY() - c.getY()))
+	// 	/ ((b.getY() - c.getY()) * (a.getX() - c.getX()) + (c.getX() - b.getX()) * (a.getY() - c.getY()));
+	// gamma = Fixed(1.0f) - alpha - beta;
+	
+	return (alpha > 0 && beta > 0 && gamma > 0);
 }
--- CPP02/ex03/srcs/main.cpp
+++ CPP02/ex03/srcs/main.cpp
@@ -4,9 +4,14 @@ bool    bsp(Point const a, Point const b, Point const c, Point const point);
 
 int main(void)
 {
-    Point	a(4.75, 5.03);
+    Point	a( 4.75, 5.03 );
+	Point	b( 20.0, 10.0 );
+	Point	c( 6.84, 2.5 );
+	bool	valid;
 
-    std::cout << a.getX() << std::endl;
-    std::cout << a.getY() << std::endl;
+	valid = bsp(a, b, c, Point(10, 10));
+
+    std::cout << valid << std::endl;
+    // std::cout << a.getY() << std::endl;
     return 0;
 }
