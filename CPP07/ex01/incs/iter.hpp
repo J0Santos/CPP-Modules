3bb9910425f44657c7b1baf802db97f2d0b4fef4
J0Santos <zepedroabsantos@gmail.com>
2022-09-30 14:15:36 +0100

    done CPP07/ex01 starting cpp07/ex02

--- CPP07/ex01/incs/iter.hpp
+++ CPP07/ex01/incs/iter.hpp
@@ -2,18 +2,21 @@
 # define ITER_HPP
 
 # include <iostream>
+#include "Colors.hpp"
 
 # define LOG(x) std::cout << x << std::endl
 
 template < typename T >
 void	iter(T* array, int size, void(*f)(T const&)) {
+	LOG(GREEN << "using int type" << RESET);
 	for (int i = 0; i < size; i++)
 		f(array[i]);
 }
 
 template<typename T>
-void	iter(T* array, int size, void(*f)(T &)) {
-	for (int i = 0; i < size; i++)
+void	iter(T* array, size_t size, void(*f)(T const&)) {
+	LOG(GREEN << "using size_t type" << RESET);
+	for (size_t i = 0; i < size; i++)
 		f(array[i]);
 }
 
--- CPP07/ex01/srcs/main.cpp
+++ CPP07/ex01/srcs/main.cpp
@@ -1,17 +1,39 @@
 #include "iter.hpp"
+#include <cstdio>
 
 template< typename T >
 void print( T const & x ) { std::cout << x << std::endl; return; }
 
 template <typename T>
-void addZero(T &x) {
+void tenMultiplier(T &x) {
 	LOG(x * 10);
 	return ;
 }
 
+void caps(std::string const& x) {
+	for(size_t i = 0; i < x.length(); i++)
+		putchar(toupper(x[i]));
+		LOG("");
+}
+
 int main() {
-	int tab[] = { 0, 1, 2, 3, 4 };
-	iter( tab, 5, print );
-	iter( tab, 5, addZero );
-	return 0;
+	{
+		LOG(YELLOW << "Testing int array" << RESET);
+		int tab[] = { 0, 1, 2, 3, 4 };
+		size_t i = 5;
+
+		iter( tab, 5, print );
+		iter( tab, 5, tenMultiplier );
+		iter(tab, i, tenMultiplier);
+	}
+	{
+		LOG(YELLOW << "Testing string array" << RESET);
+		std::string tab2[] = {"Hello", "goodbye"};
+		iter(tab2, 2, caps);
+	}
+	{
+		LOG(YELLOW << "Testing float array" << RESET);
+		float tab3[] = {23.23, 234.2, 1.5};
+		iter(tab3, 3, tenMultiplier);
+	}
 }
