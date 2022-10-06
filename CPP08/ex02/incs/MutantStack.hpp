313d80287a665187319622895eceb13301117128
J0Santos <zepedroabsantos@gmail.com>
2022-10-06 17:39:50 +0100

    done CPP08/ex02

--- CPP08/ex02/incs/MutantStack.hpp
+++ CPP08/ex02/incs/MutantStack.hpp
@@ -3,76 +3,37 @@
 
 # include <iostream>
 # include <stack>
-# include <iterator>
+# include <deque>
+# include <vector>
 # include "Colors.hpp"
 
 # define LOG(x) std::cout << x << std::endl
 
-template <typename T>
+template < typename T, class Container = std::deque<T> >
 class MutantStack : public std::stack<T> {
 
-private:
-
-public:
-	MutantStack( void ) : std::stack<T>() {}
-	~MutantStack( void ) {}
-	MutantStack( MutantStack const& src ): std::stack<T>(src) {}
-
-	MutantStack	&operator=( MutantStack const& rhs ) {
-		std::stack<T>::operator=(rhs);
-		return (*this);
-	}
-	class iterator : public std::iterator<std::bidirectional_iterator_tag, T> {
-	
-	private:
-		T*	pointer;
-		
 	public:
-		iterator(void) : pointer(0) {}
-		iterator(T* x) : pointer(x) {}
-		iterator(iterator const& src): pointer(src.pointer) {}
-		
-		iterator& operator=(iterator const& rhs) {
-			this->pointer = rhs.pointer;
+		typedef typename std::stack<T>::container_type::iterator	iterator;
+
+		MutantStack() {}
+		MutantStack( MutantStack const& src ) {
+			*this = src;
+		}
+		~MutantStack() {}
+		MutantStack& operator = (const MutantStack &rhs) {
+			this->c = rhs.c;
 			return (*this);
 		}
 
-		iterator& operator++(void) {
-			++pointer;
-			return (*this);
-		}
-		iterator operator++(T) {
-			iterator	temp(*this);
-			operator++();
-			return (temp);
-		}
-		iterator& operator--(void) {
-			--pointer;
-			return (*this);
-		}
-		iterator operator--(T) {
-			iterator	temp(*this);
-			operator--();
-			return (temp);
+		iterator	begin() {
+			return (this->c.begin());
 		}
 
-		bool	operator==(iterator const& rhs) const {
-			return(pointer == rhs.p);
-		}
-		bool	operator!=(iterator const& rhs) const {
-			return(pointer != rhs.p);
+		iterator	end() {
+			return (this->c.end());
 		}
 
-		int& operator*(void) {
-			return (*pointer);
-		}
-	};
-	iterator	begin(void) {
-		return iterator(*this);
-	}
-// 	iterator	end(void) {
-
-// 	}
 };
+
 
 #endif /* MUTANTSTACK_HPP */
--- CPP08/ex02/srcs/main.cpp
+++ CPP08/ex02/srcs/main.cpp
@@ -1,29 +1,81 @@
 #include "MutantStack.hpp"
+#include <list>
+
+#define LOG_S(x) std::cout << CYAN << x << RESET << std::endl
 
 int	main( int argc, char** argv ) {
 
 	(void)argc;
 	(void)argv;
 
-	MutantStack<int>	myStack;
+	{
+		LOG("TEST 1");
+		MutantStack<int>	myStack;
+		std::list<int>		list;
+	
+		myStack.push(5);
+		myStack.push(17);
+		myStack.push(3);
+		myStack.push(14);
+		myStack.push(737);
+		myStack.push(0);
+		list.push_front(5);
+		list.push_front(17);
+		list.push_front(3);
+		list.push_front(14);
+		list.push_front(737);
+		list.push_front(0);
 
-	myStack.push(5);
-	myStack.push(17);
+		LOG("Top of stack: " << myStack.top());
+		LOG_S("Top of list: " << list.front());
+		LOG("Size of stack: " << myStack.size());
+		LOG_S("Size of list: " << list.size());
 
-	LOG(BLUE << "Top of stack: " << RESET << myStack.top());
-	LOG(BLUE << "Size of stack: " << RESET << myStack.size());
+		LOG("");
+		LOG("");
+		MutantStack<int>::iterator	it = myStack.begin();
+		MutantStack<int>::iterator	ite = myStack.end();
+		LOG("it: " << *it);
+		LOG("ite " << *ite);
+		it++;
+		LOG("it: " << *it);
+		it--;
+		LOG("it: " << *it);
+	}
+	{
+		LOG("");
+		LOG("");
+		LOG("TEST 2");
+		MutantStack<std::string>	myStack;
+		std::list<std::string>		list;
 
-	myStack.push(3);
-	myStack.push(5);
-	myStack.push(737);
-	myStack.push(0);
+		myStack.push("hello");
+		myStack.push("test");
+		myStack.push("third");
+		myStack.push("forth");
+		myStack.push("bajoras");
+		myStack.push("zero");
+		list.push_front("hello");
+		list.push_front("test");
+		list.push_front("third");
+		list.push_front("forth");
+		list.push_front("bajoras");
+		list.push_front("zero");
 
-	MutantStack<int>::iterator it = myStack.begin();
-	// MutantStack<int>::iterator ite = myStack.end();
+		LOG("Top of stack: " << myStack.top());
+		LOG_S("Top of list: " << list.front());
+		LOG("Size of stack: " << myStack.size());
+		LOG_S("Size of list: " << list.size());
 
-	LOG(BLUE << "it begin: " << RESET << *it);
-	// LOG(BLUE << "it end: " << RESET << *ite);
+		LOG("");
+		LOG("");
+		for (MutantStack<std::string>::iterator it = myStack.begin(); it != myStack.end(); it++) {
+			LOG(*it);
+		}
+	}
 
-	// it++;
-	// LOG(BLUE << "it: " << RESET << *it);
+
+
+
+
 }
